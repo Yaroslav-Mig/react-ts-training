7716b033c041a922498d8debb534c8cdc3055d1f
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2022-10-14 10:56:09 +0300

    refact: add useRef and createRef

--- src/components/useRef/useRefDemo.tsx
+++ src/components/useRef/useRefDemo.tsx
@@ -1,9 +1,14 @@
-import React, { ChangeEvent, useRef, useState } from 'react';
+import React, { ChangeEvent, createRef, useEffect, useRef, useState } from 'react';
 import st from './useRefDemo.module.css';
 
 const useRefDemo = () => {
   console.log('useRefDemo');
-  return <InputToggleButtonMemo />;
+  return (
+    <>
+      <InputToggleButtonMemo />
+      <CreateRefDemo />
+    </>
+  );
 };
 export default React.memo(useRefDemo);
 
@@ -12,6 +17,11 @@ const InputToggleButton = () => {
 
   const [value, setValue] = useState<string>('');
   const inputEl = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const el = inputEl.current as HTMLInputElement;
+    el.focus();
+  }, []);
 
   const BlurHandler = () => {
     const el = inputEl.current as HTMLInputElement;
@@ -37,3 +47,39 @@ const InputToggleButton = () => {
   );
 };
 const InputToggleButtonMemo = React.memo(InputToggleButton);
+
+// Compiled with --strictNullChecks
+export const CreateRefDemo = () => {
+  const [counter, setCounter] = useState<number>(0);
+
+  const createRefExp = createRef<number | null>();
+  const useRefExp = useRef<number | null>(0);
+  console.log(createRefExp, '1');
+
+  const onClickHandler = () => setCounter(counter + 1);
+  if (!createRefExp) {
+    //@ts-ignore
+    createRefExp.current = counter;
+    console.log(createRefExp, '2');
+    // createRefExp['current']['style']['color'] = 'red';
+    // createRefExp.current.style.color = 'red';
+  }
+  if (!useRefExp.current) {
+    useRefExp.current = counter;
+  }
+
+  return (
+    <section className='box'>
+      <div className='values'>
+        <span>createRef value: {createRefExp.current}</span>
+      </div>
+      <div className='values'>
+        <span>useRef value: {useRefExp.current}</span>
+      </div>
+      <div className='values'>
+        <span>This component has been rendered {counter} times. </span>
+      </div>
+      <button onClick={onClickHandler}>Add a render!</button>
+    </section>
+  );
+};
